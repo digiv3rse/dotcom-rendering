76e9f2c21634cf0332efa2e69fdee4c3a9fb7f92
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2024-04-18 12:50:42 +0100

    Fixed some changes made by the auto-migration
    - Removed `autodocs`
    - Removed ranges from dependencies
    - Moved `@storybook/addon-webpack5-compiler-swc` into `dependencies` from `devDependencies`

--- dotcom-rendering/.storybook/main.ts
+++ dotcom-rendering/.storybook/main.ts
@@ -82,9 +82,6 @@ const config: StorybookConfig = {
 		name: '@storybook/react-webpack5',
 		options: { fastRefresh: true },
 	},
-	docs: {
-		autodocs: true,
-	},
 };
 
 /** the webpack.Configuration type from Storybook */
